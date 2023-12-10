ed5ca80f4d89fb54161fe75b2f71241324fb6024
Clément Vandendaelen <vandendaelenclement@gmail.com>
2023-12-10 14:18:29 +0100

    Fixed Providers serialization

--- src/main/kotlin/com/vandeas/dto/enums/Providers.kt
+++ src/main/kotlin/com/vandeas/dto/enums/Providers.kt
@@ -3,9 +3,14 @@ package com.vandeas.dto.enums
 import com.vandeas.service.Mailer
 import com.vandeas.service.impl.mailer.ResendMailer
 import com.vandeas.service.impl.mailer.SendGridMailer
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 enum class Providers {
+    @SerialName("SENDGRID")
     SENDGRID,
+    @SerialName("RESEND")
     RESEND
 }
 
